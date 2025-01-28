5ad2d79ad545aad201ec17267cff43537f9b5744
azenizzka <azenizzka@gmail.com>
2025-01-29 03:06:34 +0700

    feat: add new dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,61 @@
-plugins {
-    application
-}
-
 repositories {
-    mavenCentral()
-}
-
-dependencies {
-    //
-}
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(21)
-    }
+  mavenCentral()
 }
 
 application {
-    mainClass = "ru.nskopt.App"
+  mainClass = "ru.nskopt.App"
+}
+
+plugins {
+  application
+
+  id("org.springframework.boot") version "3.4.2"
+  id("io.spring.dependency-management") version "1.1.7"
+  id("com.diffplug.spotless") version "6.25.0"
+}
+
+dependencies {
+  implementation("org.springframework.boot:spring-boot-starter-web")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+  runtimeOnly("org.postgresql:postgresql")
+
+
+  compileOnly("org.projectlombok:lombok")
+  annotationProcessor("org.projectlombok:lombok")
+}
+
+configurations {
+  compileOnly {
+    extendsFrom(configurations.annotationProcessor.get())
+  }
+}
+
+spotless {
+  java {
+    googleJavaFormat()
+    removeUnusedImports()
+  }
+}
+
+repositories {
+  mavenCentral()
+}
+
+java {
+  toolchain {
+    languageVersion = JavaLanguageVersion.of(21)
+  }
+}
+
+tasks.named("bootJar") {
+  dependsOn("spotlessApply")
+}
+
+tasks.named("bootRun") {
+  dependsOn("spotlessApply")
+}
+
+tasks.named("build") {
+  dependsOn("spotlessApply")
 }
