2df03dd8b14abdbe1a2b3b41d7b3edb39d389705
azenizzka <azenizzka@gmail.com>
2025-02-10 07:22:06 +0700

    feat: add all args constructor

--- src/main/java/ru/nskopt/models/entities/Cost.java
+++ src/main/java/ru/nskopt/models/entities/Cost.java
@@ -5,11 +5,13 @@ import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Embeddable
 public class Cost {
   @PositiveOrZero
