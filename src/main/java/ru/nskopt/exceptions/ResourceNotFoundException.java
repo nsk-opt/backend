7e48aa0607c25eace0d93a29f95e2259a7005610
azenizzka <azenizzka@gmail.com>
2025-02-13 05:41:42 +0700

    refactor: delete unused constructor

--- src/main/java/ru/nskopt/exceptions/ResourceNotFoundException.java
+++ src/main/java/ru/nskopt/exceptions/ResourceNotFoundException.java
@@ -1,10 +1,6 @@
 package ru.nskopt.exceptions;
 
 public class ResourceNotFoundException extends RuntimeException {
-  public ResourceNotFoundException(String message) {
-    super(message);
-  }
-
   public ResourceNotFoundException(Long id) {
     super("Resource with id " + id + " not found");
   }
