d0376623246c943e1cb1d566a42cf3dda3364e1b
azenizzka <azenizzka@gmail.com>
2025-02-26 08:23:16 +0700

    test: add test for unsupported content-type, get image

--- src/test/java/ru/nskopt/controllers/ImageControllerTest.java
+++ src/test/java/ru/nskopt/controllers/ImageControllerTest.java
@@ -1,10 +1,13 @@
 package ru.nskopt.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.io.File;
 import java.nio.file.Files;
+import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.nskopt.App;
+import ru.nskopt.models.entities.Image;
 import ru.nskopt.repositories.ImageRepository;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = App.class)
@@ -96,5 +100,38 @@ class ImageControllerTest {
     Long imageId = Long.parseLong(responseContent);
 
     assertTrue(imageRepository.findById(imageId).isPresent());
+  }
+
+  @Test
+  void createImage_unsupported_format() throws Exception {
+    String filePath = "src/test/resources/images/image.gif";
+    File file = new File(filePath);
+
+    byte[] fileContent = Files.readAllBytes(file.toPath());
+
+    MockMultipartFile multipartFile =
+        new MockMultipartFile("file", file.getName(), "image/gif", fileContent);
+
+    mockMvc
+        .perform(multipart("/api/images").file(multipartFile))
+        .andExpect(status().isUnsupportedMediaType());
+  }
+
+  @Test
+  void getImage_successful() throws Exception {
+    Image image = new Image();
+    image.setData("data".getBytes());
+
+    imageRepository.save(image);
+
+    byte[] result =
+        mockMvc
+            .perform(get("/api/images/" + image.getId()).contentType("image/webp"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsByteArray();
+
+    assertTrue(Arrays.equals(result, image.getData()));
   }
 }
