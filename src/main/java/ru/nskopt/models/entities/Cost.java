d90a5b6bafd718d4e24b001092e9378ded1726ca
azenizzka <azenizzka@gmail.com>
2025-03-01 23:19:06 +0700

    fix: add check for null wholesale, retail cost

--- src/main/java/ru/nskopt/models/entities/Cost.java
+++ src/main/java/ru/nskopt/models/entities/Cost.java
@@ -1,10 +1,11 @@
 package ru.nskopt.models.entities;
 
+import java.math.BigDecimal;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
-import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,10 +15,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Embeddable
 public class Cost {
+  @NotNull(message = "Wholesale price must exists")
   @PositiveOrZero(message = "Wholesale cost must be positive or zero")
   @Schema(description = "Оптовая стоимость в рублях", example = "1347")
   private BigDecimal wholesalePrice;
 
+  @NotNull(message = "Retail price must exists")
   @Positive(message = "Retail cost must be positive")
   @Schema(description = "Розничная стоимость в рублях", example = "4296")
   private BigDecimal retailPrice;
